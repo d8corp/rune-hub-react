2d6d7e629de0c1be3a58dcfeecbdd7777698c79e
Deight <d8corp@mail.ru>
2026-07-14 13:22:53 +0300

    Change readme

--- src/hooks/useHub/useHub.ts
+++ src/hooks/useHub/useHub.ts
@@ -2,6 +2,25 @@ import { useContext } from 'react'
 
 import { HubContext } from '../../context'
 
+/**
+ * Returns the current Hub instance from context.
+ *
+ * If no Hub is provided via HubProvider, returns the default `Hub.root` instance.
+ *
+ * @returns The current Hub instance
+ * @see {@link https://github.com/d8corp/rune-hub-react#usehub}
+ *
+ * @example
+ * ```tsx
+ * function MyComponent () {
+ *   const hub = useHub()
+ *
+ *   console.log(hub)
+ *
+ *   return <h1>Hello World!</h1>
+ * }
+ * ```
+ */
 export function useHub () {
   return useContext(HubContext)
 }
