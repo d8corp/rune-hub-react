332c911c7d721371deaed8fd42fd379add49ea2e
Deight <d8corp@mail.ru>
2026-07-14 03:13:43 +0300

    Fix useRune

--- src/hooks/useRune/useRune.ts
+++ src/hooks/useRune/useRune.ts
@@ -1,19 +1,16 @@
-import { useCallback, useSyncExternalStore } from 'react'
+import { useCallback, useEffect, useMemo, useSyncExternalStore } from 'react'
 import type { Rune } from 'rune-hub'
 
 import { useSlot } from '../useSlot'
 
 export function useRune<T extends Rune> (rune: T): ReturnType<T> {
   const slot = useSlot<ReturnType<T>>(rune)
+  const stop = useMemo(() => slot.on(), [slot])
+
+  useEffect(() => stop, [stop])
 
   const subscribe = useCallback((callback: () => void) => {
-    const destroy = slot.on('change', callback)
-    const off = slot.on()
-
-    return () => {
-      off()
-      destroy()
-    }
+    return slot.on('change', callback)
   }, [slot])
 
   return useSyncExternalStore<ReturnType<T>>(
--- src/index.test.tsx
+++ src/index.test.tsx
@@ -130,4 +130,64 @@ describe('Examples', () => {
 
     expect(container.querySelector('li')?.style.textDecoration).toBe('none')
   })
+
+  test('Effect', () => {
+    const log: number[] = []
+    const count = () => 0
+    const show = () => true
+
+    const increase = () => {
+      set(count, get(count) + 1)
+    }
+
+    const effect = () => {
+      log.push(get(count))
+    }
+
+    function Counter () {
+      const value = useRune(count)
+      // Subscribe to count changes
+
+      useRune(effect)
+      // Activate log effect
+
+      return <button onClick={increase}>Count: {value}</button>
+    }
+
+    function App () {
+      const value = useRune(show)
+
+      return value ? <Counter /> : null
+    }
+
+    const { container } = render(<App />)
+
+    expect(container.innerHTML).toBe('<button>Count: 0</button>')
+    expect(log).toEqual([0])
+
+    act(() => {
+      container.querySelector('button')!.click()
+    })
+
+    expect(container.innerHTML).toBe('<button>Count: 1</button>')
+    expect(log).toEqual([0, 1])
+
+    act(() => {
+      container.querySelector('button')!.click()
+    })
+
+    expect(container.innerHTML).toBe('<button>Count: 2</button>')
+    expect(log).toEqual([0, 1, 2])
+
+    act(() => {
+      set(show, false)
+    })
+
+    act(() => {
+      increase()
+    })
+
+    expect(container.innerHTML).toBe('')
+    expect(log).toEqual([0, 1, 2])
+  })
 })
